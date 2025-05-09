9863c9f6e79f8b942d83b7d36c9c3a10ca177659
TurjoSiam <t.siam24@gmail.com>
2025-05-09 23:33:50 +0600

    [ADDED]- user data from database fetched successfully

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,9 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 const Dashboard = () => {
+
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/users")
+            .then(res => setUser(res.data))
+    }, [])
+
     return (
         <div>
             <h2>this is dashboard page</h2>
+            <h2>total number of users are {user.length}</h2>
         </div>
     );
 };
