97bd4d3033274a58d8de6ec9dac2f614a11e0c32
TurjoSiam <t.siam24@gmail.com>
2025-05-10 22:46:32 +0600

    [UPDATED]-sidebar routes updated with active tab state change

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -28,8 +28,9 @@ const Dashboard = () => {
                         <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
                         <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
                             {/* Sidebar content here */}
-                            <li><a>Sidebar Item 1</a></li>
-                            <li><a>Sidebar Item 2</a></li>
+                            <li onClick={setActiveTab("dashboard")}>Statistics</li>
+                            <li onClick={setActiveTab("users")}>All Users</li>
+                            <li onClick={setActiveTab("posts")}>All posts</li>
                         </ul>
                     </div>
                 </div>
