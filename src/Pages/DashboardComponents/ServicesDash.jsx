ad8e37407742817cf3fb76397de493364c713f2d
TurjoSiam <t.siam24@gmail.com>
2025-05-12 00:14:00 +0600

    [ADDED]-services fetched from database to dashboard

--- src/Pages/DashboardComponents/ServicesDash.jsx
+++ src/Pages/DashboardComponents/ServicesDash.jsx
@@ -1,9 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 const ServicesDash = () => {
+
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://assignment-11-beta.vercel.app/allservices")
+            .then(res => setServices(res.data))
+    }, [])
+
     return (
         <div>
-            <h2>this is all services page from dashboard</h2>
+            <h2>this is all services page from dashboard. total number of services {services.length}</h2>
         </div>
     );
 };
